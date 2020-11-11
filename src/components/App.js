70b662ff0254a0dc7177076f3f72bd9732f94267
Will Ulman <will.ulman@gmail.com>
2020-11-11 10:36:11 -0500

    Import required data

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import gateImage from "../data/golden-gate.jpg";
+import placeholderImage from "../data/placeholder.jpg";
+const data = require("../data/data.json");
+
+console.log(data);
 
 const App = () => {
-  return <div>App!</div>;
+  return (
+    <div>
+      App!
+      <img src={gateImage} alt="Golden Gate Bridge" />
+      <img src={placeholderImage} alt="Placeholder" />
+    </div>
+  );
 };
 
 export default App;
